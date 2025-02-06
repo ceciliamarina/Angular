6e7c763032e69f8f0e50f9f696924304af61e306
Cecilia <netocecilia@gmail.com>
2025-02-06 07:35:08 -0300

    Primeiro commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  //templateUrl: './app.component.html',
+  template: `<h1>Hello world!</h1>`,
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'homes';
+}
