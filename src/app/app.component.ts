f46a1c23754080195acc23697925d0b6680d99d7
Cecilia <netocecilia@gmail.com>
2025-02-07 17:29:46 -0300

    version1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    HomeComponent, 
+  ],
   //templateUrl: './app.component.html',
-  template: `<h1>Hello world!</h1>`,
-  styleUrl: './app.component.css'
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`,
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'homes';
